3ce9fbf03e4de6d8330fc188a868a11efd818d71
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2022-08-08 00:42:56 +0700

    [Auth Service] - Bcrypt

--- service/authService.js
+++ service/authService.js
@@ -1,25 +1,104 @@
-const {Employee} = require('../models');
+const {Employee, User} = require('../models');
+const bcrypt = require('bcryptjs');
+const SALT = 10;
 
 const authService = {
     employeeLogin: async ({username, password}) => {
-        console.log({username, password});
         const findEmployee = await Employee.scope('withPassword').findOne({
             where: {
                 username
             },
-
         });
-        console.log(findEmployee);
 
         if (!findEmployee) {
-            throw new Error('Employee not found');
+            throw new Error('Employee not found', 404);
         }
 
-        if (findEmployee.password !== password) {
-            throw new Error('Password is incorrect');
+        const isValid = await this.comparePassword(password, findEmployee.password);
+        if (!isValid) {
+            throw new Error('Invalid password', 401);
         }
+
+        findEmployee.password = undefined
 
         return findEmployee
+    },
+    employeeRegister: async (params) => {
+        const {username, password} = params
+        // check existing username
+        const findEmployee = await Employee.findOne({
+            where: {
+                username
+            },
+        });
+
+        if (findEmployee) {
+            throw new Error('Username already exists', 404);
+        }
+
+        // hash password
+        const hashPassword = await this.hashPassword(password);
+
+        return await Employee.create({
+            ...params,
+            password: hashPassword,
+        });
+    },
+    userInAppLogin: async ({username, password}) => {
+        const findUser = await User.scope('withPassword').findOne({
+            where: {
+                username
+            },
+        });
+
+        if (!findUser) {
+            throw new Error('User not found');
+        }
+
+        const isValid = await this.comparePassword(password, findUser.password);
+        if (!isValid) {
+            throw new Error('Invalid password');
+        }
+
+        findUser.password = undefined;
+
+        return findUser
+    },
+    userRegister: async (params) => {
+        const {username, password} = params
+        const findExistUser = await User.findOne({
+            where: {
+                username
+            }
+        })
+
+        if (findExistUser) {
+            throw new Error('Username already exists');
+        }
+
+        const hashPassword = await this.hashPassword(password);
+
+        return await User.create({
+            ...params,
+            password: hashPassword,
+        });
+    },
+
+    hashPassword: async (password) => {
+        return await bcrypt.hash(password, SALT, async (err, hash) => {
+            if (err) {
+                throw new Error(err);
+            }
+            return hash;
+        });
+    },
+    comparePassword: async (password, hash) => {
+        return await bcrypt.compare(password, hash, async (err, isMatch) => {
+            if (err) {
+                throw new Error(err);
+            }
+            return isMatch;
+        });
     }
 }
 
