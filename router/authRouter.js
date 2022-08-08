b6e0d76a8e1a9f171db48219e1f69bba827064f7
Phat Dat <datp0628@gmail.com>
2022-08-08 14:11:27 +0700

    commit

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,38 +1,34 @@
 const express = require("express");
 
 const { authController } = require("../controller");
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res) => {});
 
-});
+router.get("/:id", async (req, res) => {});
 
-router.get("/:id", async (req, res) => {
+router.post("/", async (req, res) => {});
 
-});
+router.put("/:id", async (req, res) => {});
 
-router.post("/", async (req, res) => {
+router.delete("/:id", async (req, res) => {});
 
-});
+router.post(
+    "/employee-login",
+    asyncHandler(async (req, res) => {
+        const { username, password } = req.body;
 
-router.put("/:id", async (req, res) => {
+        const result = await authController.employeeLogin({
+            username,
+            password,
+        });
 
-});
-
-router.delete("/:id", async (req, res) => {
-
-});
-
-router.post('/employee-login', asyncHandler(async (req, res) => {
-    const {username, password} = req.body;
-
-    const result = await authController.employeeLogin({username, password});
-
-    res.json({
-        result,
+        res.json({
+            result,
+        });
     })
-}))
+);
 
 module.exports = router;
--- router/themeRouter.js
+++ router/themeRouter.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const router = express.Router();
+
 const { themeController } = require("../controller");
 
 module.exports = router;
